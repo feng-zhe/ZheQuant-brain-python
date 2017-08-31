25f24a39be6e8a68ef99d292cefc059dea6fecbc
Henry Feng <henryzhefeng@gmail.com>
2017-08-31 23:20:52 +0800

    tiny

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pika
 import time
+import json
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
 channel = connection.channel()
@@ -8,7 +9,9 @@ channel.queue_declare(queue='job-todo', durable=True)
 print(' [*] Waiting for messages. To exit press CTRL+C')
 
 def callback(ch, method, properties, body):
-    print(" [x] Received %r" % body)
+    print(" [x] Received message %r" % body)
+    msg = json.loads(body)
+    # todo: parse msg["cmd"]
     ch.basic_ack(delivery_tag = method.delivery_tag)
 
 channel.basic_qos(prefetch_count=1) # only accept one message
