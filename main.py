82f4047400b66424fbf949a63895f19219a6b30e
Henry Feng <henryzhefeng@gmail.com>
2017-09-02 12:45:23 +0800

    can read from queue

--- main.py
+++ main.py
@@ -2,23 +2,33 @@ import pika
 import time
 import json
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
-channel = connection.channel()
+def main():
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
+    channel = connection.channel()
+    channel.queue_declare(queue='jobs-todo', durable=True) # declare the queue
+    print(' [*] Waiting for messages. To exit press CTRL+C')
 
-channel.queue_declare(queue='job-todo', durable=True)
-print(' [*] Waiting for messages. To exit press CTRL+C')
+    def callback(ch, method, properties, body):
+        print(' [x] Received message %r' % body)
+        msg = json.loads(body.decode('utf-8'))
+        cmd_strs = msg["cmd"].split()
+        print(' cmd_strs is: %r' % cmd_strs)
+        for cmd_str in cmd_strs:
+            # todo: parse msg["cmd"]
+            # e.g. schedule -n JOB_NAME -dsc "JOB_DESC" -t JOB_TYPE -p "JOB_PARAMETERS"
+            pass
+        ch.basic_ack(delivery_tag = method.delivery_tag)
 
-def callback(ch, method, properties, body):
-    print(" [x] Received message %r" % body)
-    msg = json.loads(body)
-    cmd_strs = msg["cmd"].split()
-    for cmd_str in cmd_strs:
-        # todo: parse msg["cmd"]
-        # e.g. schedule -n JOB_NAME -dsc "JOB_DESC" -t JOB_TYPE -p "JOB_PARAMETERS"
-        pass
-    ch.basic_ack(delivery_tag = method.delivery_tag)
+    channel.basic_qos(prefetch_count=1) # only accept one message
+    channel.basic_consume(callback, queue='jobs-todo') # need acknowledge
 
-channel.basic_qos(prefetch_count=1) # only accept one message
-channel.basic_consume(callback, queue='task_queue') # need acknowledge
+    channel.start_consuming()
 
-channel.start_consuming()
+if __name__ == '__main__':
+    while True:
+        try:
+            main()
+        except Exception as e:
+            print('Exception raised when try to connect:', e)
+            print('Retry a few seconds later')
+            time.sleep(5)
