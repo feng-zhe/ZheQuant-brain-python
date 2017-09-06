dca9674de3d1ec5416a95cf2b60c50fdbde83dba
Henry Feng <henryzhefeng@gmail.com>
2017-09-06 08:24:38 +0800

    daily commit

--- zq_calc/calc_mgr.py
+++ zq_calc/calc_mgr.py
@@ -4,12 +4,12 @@ def calc_mgr(cmd_dict, cb):
     MOVING_AVERAGE_STR = 'mv_avg' # moving average
     INC_PERCENT_STR = 'inc_pct' # increasement by percentage
     rst = ''
-    calc_type = cmd_dict['t']
+    calc_type = cmd_dict['-t']
     if  calc_type == MOVING_AVERAGE_STR:
         print('[x] Identify calculation for moving average')
-        rst = mv_avg(cmd_dict)
+        rst = mv_avg(cmd_dict['-p'][1:-1]) # remove the double quotes first
         cb(rst)
-    elif calc_type == MOVING_AVERAGE_STR:
+    elif calc_type == INC_PERCENT_STR:
         # TODO
         pass
     else:
--- zq_calc/mv_avg.py
+++ zq_calc/mv_avg.py
@@ -1,2 +1,16 @@
+from zq_db.mongodb import get_stock_data
+
 def mv_avg(cmd_dict):
+    '''Calculate the top stocks ranked by moving average
+
+    Args:
+        cmd_dict:   A mapping containing calculation parameters.
+                    -n: number of stocks return
+                    -d: days range used when calculating moving average
+    Returns:
+        A list of dicts representing the 5
+
+    Raises:
+        
+    '''
     pass # TODO
