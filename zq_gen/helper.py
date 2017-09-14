b1ee56e90d7638d6f934cc0d207f0849eeee40ff
fengzhe <henryzhefeng@gmail.com>
2017-09-14 17:28:45 +0800

    fix two bug, left one in helper.py

--- zq_gen/helper.py
+++ zq_gen/helper.py
@@ -4,11 +4,13 @@ Helper functions
 
 def cmd_str2dic(cmd_str):
     words = cmd_str.split()
-    count = len(a)
+    count = len(words)
     rst = {}
     if count>=1:
         rst['cmd_name'] = words[0]
         i = 1
+        # TODO: refactor to use state machine
         while i+1<count:
             rst[words[i]] = words[i+1]
+            i+=2
     return rst
