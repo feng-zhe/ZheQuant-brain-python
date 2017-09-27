15c2595e126689d184a5de52b8f209b4e3e6eb67
Henry Feng <henryzhefeng@gmail.com>
2017-09-27 22:36:48 +0800

    add a test for json

--- zq_gen/str.py
+++ zq_gen/str.py
@@ -13,19 +13,29 @@ def cmd_str2dic(cmd_str):
             begin = 1
         curr_word = '...'                       # default parameter
         quoted = False
+        curly_braced = False
         for word in words[begin:]:
-            if quoted:                          # currently expecting the reverse double quote
+            if quoted:                          # expecting the reverse double quote
                 if word.endswith('"'):
                     quoted = False
                     word = word[:-1]
+            elif curly_braced:                  # expecting the reverse curly brace
+                if word.endswith('}'):
+                    curly_braced = False
             else:
                 if word[0:1]=='-':              # a new parameter
                     curr_word = word
                     rst[curr_word] = ''
                     continue
-                if word.startswith('"'):
-                    quoted = True
-                    word = word[1:]
+                if word.startswith('"'):        # meet double quote
+                    if word.endswith('"'):
+                        word = word[1:-1]
+                    else:
+                        quoted = True
+                        word = word[1:]
+                elif word.startswith('{'):      # meet curly brace
+                    if not word.endswith('}'):
+                        curly_braced = True
                                                 # append to current parameter
             if len(rst[curr_word]) == 0:        # first value 
                 rst[curr_word] += word
@@ -35,20 +45,30 @@ def cmd_str2dic(cmd_str):
 
 # Unit test class
 class TestString(unittest.TestCase):
-    def test_primary_cmd(self):
-        cmd_str = 'schedule -n job name -dsc job description -t job_type -p "-d 20 -n 5"'
+    def test_json(self):
+        cmd_str  = 'command -t job_type -p {"num1":1, "num2":2, "str1":"abcd", "str2":"efgh"}'
         cmd_dict = cmd_str2dic(cmd_str)
         exp_dict = {
-                'cmd_name': 'schedule',
-                '-n':       'job name',
-                '-dsc':     'job description',
-                '-t':       'job_type',
-                '-p':       '-d 20 -n 5'
+                'cmd_name' : 'command',
+                '-t'       : 'job_type',
+                '-p'       : '{"num1":1, "num2":2, "str1":"abcd", "str2":"efgh"}'
+                }
+        self.assertEqual(cmd_dict, exp_dict)
+
+    def test_primary_cmd(self):
+        cmd_str  = 'schedule -n job name -dsc job description -t job_type -p "-d 20 -n 5"'
+        cmd_dict = cmd_str2dic(cmd_str)
+        exp_dict = {
+                'cmd_name' : 'schedule',
+                '-n'       : 'job name',
+                '-dsc'     : 'job description',
+                '-t'       : 'job_type',
+                '-p'       : '-d 20 -n 5'
                 }
         self.assertEqual(cmd_dict, exp_dict)
 
     def test_mv_avg_cmd(self):
-        cmd_str = '-n 5 -d 20'
+        cmd_str  = '-n 5 -d 20'
         cmd_dict = cmd_str2dic(cmd_str)
         exp_dict = {
                 '-n':   '5',
