752cbdfb0ec696dc86a68d55adf8c8bef3b7f992
Henry Feng <henryzhefeng@gmail.com>
2017-09-28 22:32:56 +0800

    add inc_pct module

--- zq_calc/calc_mgr.py
+++ zq_calc/calc_mgr.py
@@ -1,17 +1,20 @@
 from zq_calc.mv_avg import mv_avg
+from zq_calc.inc_pct import inc_pct
 
 def calc_mgr(cmd_dict, cb):
     MOVING_AVERAGE_STR = 'mv_avg'                                       # moving average
     INC_PERCENT_STR = 'inc_pct'                                         # increasement by percentage
     rst = ''
     calc_type = cmd_dict['-t']
+    param = cmd_dict['-p']
     if  calc_type == MOVING_AVERAGE_STR:
-        print(' [x] Identified calculation for moving average')
-        rst = mv_avg(cmd_dict['-p']) 
+        print(' [x] Identified calculation for mv_avg(moving average)')
+        rst = mv_avg(param) 
     elif calc_type == INC_PERCENT_STR:
-        # TODO
-        pass
+        print(' [x] Identified calculation for inc_pct(increasement by percentage)')
+        rst = inc_pct(param)
     else:
         print(' [!]Unkown calculation type %s, skipped' % calc_type)
+        rst = 'not supported calculation'
     print(' [x] Calculation done')
     cb(rst)
---
+++ zq_calc/inc_pct.py
@@ -0,0 +1,32 @@
+import json
+
+def inc_pct(json_str):
+    '''Calculate the increasement by percentage
+
+    Args:
+        json_str:   The json of stock composition
+                    e.g.
+                    {
+                        "test_code1": 200,
+                        "test_code2": 300,
+                        ...
+                    }
+
+    Returns:
+        A number indicating the increasement percentage
+            e.g. return 1 means 1%
+
+    Raises:
+       N/A 
+    '''
+    compo = json.loads(json_str)
+    begin_value = 0                                     # begin value
+    for key, val in compo:
+        pass                                            #TODO
+
+    curr_value = 0                                      # current value
+    for key, val in compo:
+        pass                                            #TODO
+
+    if begin_value:
+        return curr_value/begin_value*100
