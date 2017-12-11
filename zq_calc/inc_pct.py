f2e24b188461951e6bef810d6e435b3ef7b12d43
Henry Feng <henryzhefeng@gmail.com>
2017-12-11 16:12:53 +0800

    refactor the inc_pct and its tests

--- zq_calc/inc_pct.py
+++ zq_calc/inc_pct.py
@@ -9,7 +9,8 @@ import zq_gen.str as zq_str
 import zq_db.mongodb as zq_mgdb
 
 def inc_pct(cmd_str):
-    '''Calculate the increasement by percentage
+    '''
+    Calculate the increasement by percentage
 
     Args:
         cmd_str:    A string of parameters
@@ -31,30 +32,17 @@ def inc_pct(cmd_str):
     Raises:
         N/A
     '''
-    cmd_dict    = zq_str.cmd_str2dic(cmd_str)
     try:
-        compo_str   = cmd_dict['-c']
-        begin_str   = cmd_dict['-b']
-        end_str     = cmd_dict['-e']
-        begin_year  = int(begin_str[0:4])
-        begin_month = int(begin_str[4:6])
-        begin_day   = int(begin_str[6:])
-        end_year    = int(end_str[0:4])
-        end_month   = int(end_str[4:6])
-        end_day     = int(end_str[6:])
+        compo, begin, end = _parse_cmd(cmd_str)
     except KeyError:
         return 'Missing parameter in command string'
     except ValueError:
         return 'Error in parsing the command string'
-    tzinfo = pytz.timezone('Asia/Shanghai')
-    begin_dt = datetime.datetime(begin_year, begin_month, begin_day, tzinfo=tzinfo)
-    end_dt = datetime.datetime(end_year, end_month, end_day, tzinfo=tzinfo)
-    compo = json.loads(compo_str)
     begin_value = 0
     end_value = 0
     for code, num in compo.items():
-        begin_doc = zq_mgdb.get_single_stock_data(code, begin_dt)
-        end_doc = zq_mgdb.get_single_stock_data(code, end_dt)
+        begin_doc = zq_mgdb.get_single_stock_data(code, begin)
+        end_doc = zq_mgdb.get_single_stock_data(code, end)
         if not begin_doc or not end_doc:
             return 'No records in specified date'
         begin_value += begin_doc['close'] * num
@@ -62,3 +50,37 @@ def inc_pct(cmd_str):
     if not begin_value:
         return 'Begin value is zero, cannot calculate'
     return round((end_value - begin_value) / begin_value, 4)
+
+def _parse_cmd(cmd_str):
+    '''
+    Parse the command string to get arguments
+
+    Args:
+        cmd_str:    A string of parameters
+
+    Returns:
+        A tuple (compo, begin, end) in which:
+        The compo is the dictionary containing composition info.
+        The begin is the datetime object for begin date.
+        The end is the datatime object for end date.
+
+    Raises:
+        KeyError:       Thrown when there is missing parameters in command string
+        ValueError:     Thrown when the input date string is not correct
+    '''
+    cmd_dict    = zq_str.cmd_str2dic(cmd_str)
+    compo_str   = cmd_dict['-c']
+    begin_str   = cmd_dict['-b']
+    end_str     = cmd_dict['-e']
+    begin_year  = int(begin_str[0:4])
+    begin_month = int(begin_str[4:6])
+    begin_day   = int(begin_str[6:])
+    end_year    = int(end_str[0:4])
+    end_month   = int(end_str[4:6])
+    end_day     = int(end_str[6:])
+    tzinfo = pytz.timezone('Asia/Shanghai')
+    begin = datetime.datetime(begin_year, begin_month, begin_day, tzinfo=tzinfo)
+    end = datetime.datetime(end_year, end_month, end_day, tzinfo=tzinfo)
+    compo = json.loads(compo_str)
+    return compo, begin, end
+
--- zq_calc/test_inc_pct.py
+++ zq_calc/test_inc_pct.py
@@ -1,20 +1,68 @@
 import unittest
 import unittest.mock as mock
+from datetime import datetime
+import pytz
 import zq_calc.inc_pct as zq_inc_pct
+
+tzinfo = pytz.timezone('Asia/Shanghai')
+
+def mock_db_func(code, date):
+    mock_data = [
+        {
+            'code'  : 'test_code_1',
+            'date'  : datetime(2017,12,11,tzinfo=tzinfo),
+            'close' : 11.36
+        },
+        {
+            'code'  : 'test_code_1',
+            'date'  : datetime(2017,12,15,tzinfo=tzinfo),
+            'close' : 13.13
+        },
+        {
+            'code'  : 'test_code_2',
+            'date'  : datetime(2017,12,11,tzinfo=tzinfo),
+            'close' : 6.10
+        },
+        {
+            'code'  : 'test_code_2',
+            'date'  : datetime(2017,12,15,tzinfo=tzinfo),
+            'close' : 5.83
+        }
+        ]
+    for item in mock_data:
+        if item['code']==code and item['date']==date:
+            return item
 
 class TestIncreasingPercentage(unittest.TestCase):
     '''
     Test increasing percentage module
     '''
     @mock.patch('zq_db.mongodb.get_single_stock_data')
-    def test_inc_pct(self, mocked_db_func):
-        mocked_db_func.side_effect = [
-                {'code':'test_code_1','close':11.36},
-                {'code':'test_code_1','close':13.13}]
-        cmd_str = '-c {"test_code_1":100} -b 20171211 -e 20171215'
+    def test_inc_pct(self, mocked_func):
+        mocked_func.side_effect = mock_db_func
+        cmd_str = '-c {"test_code_1":100, "test_code_2":200} -b 20171211 -e 20171215'
         act_rst = zq_inc_pct.inc_pct(cmd_str)
-        exp_rst = 0.1558
+        exp_rst = 0.0522
         self.assertEqual(act_rst, exp_rst)
+
+    def test_parse_cmd(self):
+        # normal case
+        cmd_str = '-c {"test_code_1":100, "test_code_2":200} -b 20171211 -e 20171215'
+        act_rst = zq_inc_pct._parse_cmd(cmd_str)
+        exp_rst = (
+                {'test_code_1':100,'test_code_2':200},
+                datetime(2017, 12, 11, tzinfo=tzinfo),
+                datetime(2017, 12, 15, tzinfo=tzinfo)
+                )
+        self.assertEqual(act_rst, exp_rst)
+        # KeyError case
+        cmd_str = '-c {"test_code_1":100, "test_code_2":200} -b 20171211'
+        with self.assertRaises(KeyError):
+            zq_inc_pct._parse_cmd(cmd_str)
+        # ValueError case
+        cmd_str = '-c {"test_code_1":100, "test_code_2":200} -b 201712aa -e 201712bb'
+        with self.assertRaises(ValueError):
+            zq_inc_pct._parse_cmd(cmd_str)
 
 if __name__ == '__main__':
     unittest.main()
